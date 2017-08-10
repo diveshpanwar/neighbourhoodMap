d03a067c10133a9eb5f9d6d6d8b83ab2225caeec
diveshpanwar <divesh.panwar@gmail.com>
2017-08-10 15:08:31 +0530

    Filter Functionality Refied

--- js/app.js
+++ js/app.js
@@ -6,13 +6,14 @@ var bounds;
 var nav = $(".nav");
 var mapContainer  = $(".map-container");
 var locationArray = [
-        {title: "Persenni Cottage", location: {lat: 31.100142, lng: 77.120649}, desc: "This is my residende.", id:0},
-        {title: "Shishu Shiksha Niketan", location: {lat: 31.10073, lng: 77.121685}, desc: "This is my first school.", id:1},
-        {title: "K.V Jutogh Cantt.", location: {lat: 31.098916, lng: 77.11202}, desc: "This is my second school.", id:2},
-        {title: "UIIT", location: {lat: 31.111849, lng: 77.134784}, desc: "This is my university.", id:3},
-        {title: "The Mall, Shimla", location: {lat: 31.104805, lng: 77.173396}, desc: "This is the famous Mall Road Shimla", id:4},
-        {title: "Jakhoo Temple", location: {lat: 31.101236, lng: 77.183877}, desc: "This is the famous Lord Hanuman Temple.", id:5},
-        {title: "Kunihar", location: {lat: 31.079539, lng: 76.961426}, desc: "This is a beautiful village located in Dist. Shimla.", id:6}
+        {title: "Shimla, Himachal Pradesh", location: {lat: 31.104815, lng: 77.173403}, desc: "This is my residende.", id:0},
+        {title: "Una, Himachal Pradesh", location: {lat: 31.468445, lng: 76.270775}, desc: "This is another district of Himachal Pradesh.", id:1},
+        {title: "Kinnaur, Himachal Pradesh", location: {lat: 31.650958, lng: 78.475195}, desc: "This is another district of Himachal Pradesh.", id:2},
+        {title: "Mandi, Himachal Pradesh", location: {lat: 31.589201, lng: 76.91821}, desc: "This is another district of Himachal Pradesh.", id:3},
+        {title: "Bilaspur, Himachal Pradesh", location: {lat: 31.340693, lng: 76.68747}, desc: "This is another district of Himachal Pradesh.", id:4},
+        {title: "Chamba, Himachal Pradesh", location: {lat: 32.553363, lng: 76.125808}, desc: "This is another district of Himachal Pradesh.", id:5},
+        {title: "Kangra, Himachal Pradesh", location: {lat: 32.099803, lng: 76.269101}, desc: "This is another district of Himachal Pradesh.", id:6},
+        {title: "Solan, Himachal Pradesh", location: {lat: 30.904486, lng: 77.096736}, desc: "This is a beautiful district of Himachal Pradesh.", id:7}
       ];
 
 var addLocation = function(data) {
@@ -22,20 +23,66 @@ var addLocation = function(data) {
   this.id = ko.observable(data.id);
 };
 
+//set the marker and add the window details...
+var setWindowMarkerandDetails = function(marker, infoWindow) {
+  infoWindow.setContent('Loading Data...');
+  var infoURL = "https://api.foursquare.com/v2/venues/search?ll="+marker.position.lat()+","+marker.position.lng()+"&query=food&v=20150609&limit=5&client_id=IT3ZFQCT45XJLD3D4ICFC1D2EL5XCHSBJ0T24Q4BICRSY0EE&client_secret=KUCRY3HL1QEXIYIX3JDCDEZIZS3ICSCYOIW5MJ4CVHYCHT44";
+  var foodPoints = "";
+    $.getJSON(infoURL, function(data) {
+      var $venues = data.response.venues;
+      if ($venues.length > 0) {
+        $venues.forEach(function(venue) {
+          foodPoints+='<li><strong>'+venue.name+'</strong></li>';
+        });
+      }else {
+        foodPoints+='<h6>Sorry! No data available for this place.</h6>';
+      }
+    }).done(function() {
+      infoWindow.setContent('<h3 class="text-center">'+marker.title+
+      '</h3><h5>Position: '+marker.position
+      +'</h5><hr class="gradient"><div><h4>Food Points</h4><ul>'
+      +foodPoints+'</ul></div>');
+    }).fail(function() {
+      infoWindow.setContent('<h3 class="text-center">'+marker.title+
+      '</h3><h5>Position: '+marker.position
+      +'</h5><hr class="gradient"><div><h4>Food Points</h4><ul>Unable to Load Data at the Moment.</ul></div>');
+    });
+
+  if(infoWindow.marker != marker) {
+     infoWindow.marker = marker;
+     // marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+     infoWindow.open(map, marker);
+     infoWindow.addListener('closeclick', function() {
+       infoWindow.marker = null;
+     });
+   }
+};
+
 var ViewModel = function() {
   var self = this;
   this.locations = ko.observableArray([]);
   locationArray.forEach(function(location) {
     self.locations.push(new addLocation(location));
   });
-  //query and filters
+  //query and filters with functionality to highlight the choosen markers with yellow
   this.query = ko.observable("");
 
   self.filterLocations = ko.dependentObservable(function() {
   		var search = this.query().toLowerCase();
   		return ko.utils.arrayFilter(self.locations(), function(location) {
+        var id = location.id();
+          if (search =="") {
+            if(markers[id])
+            markers[id].setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+          }
+          else if (location.title().toLowerCase().indexOf(search) >= 0) {
+            if(markers[id])
+              markers[id].setIcon('http://maps.google.com/mapfiles/ms/icons/yellow-dot.png');
+          }else {
+            if(markers[id])
+              markers[id].setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+          }
         return location.title().toLowerCase().indexOf(search) >= 0;
-        // console.log(location.title().toLowerCase().indexOf(search) >= 0);
   		});
   	}, this);
 
@@ -43,23 +90,15 @@ var ViewModel = function() {
   //show the info window when the list item is clicked
   self.popUpInfoWindow = function () {
     infoWindow = infoWindow || new google.maps.InfoWindow({
-       content: "dummy"
+       content: "Loading Data..."
      });
     var marker = markers[this.id()];
-         infoWindow.setContent('<div>'+marker.title+'</div><div>'+marker.position+'</div><div>'+marker.desc+'</div>');
-         if(infoWindow.marker != marker) {
-            infoWindow.marker = marker;
-            // marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
-            infoWindow.open(map, marker);
-            infoWindow.addListener('closeclick', function() {
-              infoWindow.marker = null;
-            });
-          }
-          if (window.innerWidth <=950) {
-            nav.hide();
-            nav.css("width","24%");
-            mapContainer.show();
-          }
+         setWindowMarkerandDetails(marker, infoWindow);
+         if (window.innerWidth <=950) {
+           nav.hide();
+           nav.css("width","24%");
+           mapContainer.show();
+         }
        };
 };
 
@@ -68,12 +107,14 @@ $(function() {
 
   //reload on window resize
   // $(window).resize(function(){location.reload();});
+
   //click on menu Icon
   $("#menuIcon").click(function() {
     nav.css("width","100%");
     mapContainer.hide();
     nav.show();
   });
+
   //click on the close button
   $("#close").click(function() {
     nav.css("width","24%");
@@ -90,39 +131,30 @@ function initMap() {
         mapTypeConrol: true
       });
       infoWindow = infoWindow || new google.maps.InfoWindow({
-       content: "dummy"
+       content: "Loading Data..."
      });
      bounds = new google.maps.LatLngBounds();
       for(var i = 0; i< locationArray.length; i++) {
         var position = locationArray[i].location;
         var title = locationArray[i].title;
-        var desc = locationArray[i].desc;
         var marker = new google.maps.Marker({
           position: position,
           title: title,
           animation: google.maps.Animation.DROP,
           id: i,
-          desc: desc,
           map: map
         });
         markers.push(marker);
         bounds.extend(markers[i].position);
+      }
+      markers.forEach(function(marker) {
         marker.addListener('click', function() {
           populateInfoWindow(this, infoWindow);
         });
-      }
+      });
       map.fitBounds(bounds);
     }
 //populate the info window with the information when the marker is clicked
      function populateInfoWindow(marker, infoWindow) {
-       console.log(marker);
-       infoWindow.setContent('<div>'+marker.title+'</div><div>'+marker.position+'</div><div>'+marker.desc+'</div>');
-       if(infoWindow.marker != marker) {
-          infoWindow.marker = marker;
-          // marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
-          infoWindow.open(map, marker);
-          infoWindow.addListener('closeclick', function() {
-            infoWindow.marker = null;
-          });
-        }
-     }
+        setWindowMarkerandDetails(marker, infoWindow);
+      }
